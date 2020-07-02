d826d8a7db37977c85e591bec782b63fb130b738
calvang <calvin.huang5@gmail.com>
2020-07-02 02:41:38 -0400

    County heatmap works

--- src/components/Map.js
+++ src/components/Map.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
 import apiKey from '../apiKey.json';
 import { googleMapStyles } from '../MapStyle';
+import countyData from '../dataset/countyCoords.json'
 
 export default class MapContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      hidden: false,
       globalData: {},
       countyData: [],
       heatmapData: {},
@@ -24,19 +26,145 @@ export default class MapContainer extends Component {
     }
   }
 
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.show !== this.props.show;
+  }
+
   componentWillMount() {
+    this.mapAmerica()
+    // var heatmapPositions = [];
+    // var i = {};
+    // //console.log("Initial transmission");
+    // //console.log(this.props.heatmapData);
+    // for (i of this.props.heatmapData) {
+    //   heatmapPositions.push({
+    //     lat: i.Lat,
+    //     lng: i.Lon,
+    //     weight: i.TotalConfirmed
+    //   });
+    // }
+    // // console.log(this.props.countyData)
+    // // var count = 0;
+    // // for (i of this.props.countyData) {
+    // //   heatmapPositions.push({
+    // //     lat: i.Lat,
+    // //     lng: i.Lon,
+    // //     weight: i.CurrentCases
+    // //   });
+    // //   count++;
+    // //   // if (count >) {
+    // //   //   break;
+    // //   // }
+    // // }
+    // //console.log(heatmapPositions);
+    // var heatmap = {
+    //   positions: heatmapPositions,
+    //   options: {
+    //     // gradient: [
+    //     //   'rgba(0, 255, 255, 0)',
+    //     //   'rgba(0, 255, 255, 1)',
+    //     //   'rgba(0, 191, 255, 1)',
+    //     //   'rgba(0, 127, 255, 1)',
+    //     //   'rgba(0, 63, 255, 1)',
+    //     //   'rgba(0, 0, 255, 1)',
+    //     //   'rgba(0, 0, 223, 1)',
+    //     //   'rgba(0, 0, 191, 1)',
+    //     //   'rgba(0, 0, 159, 1)',
+    //     //   'rgba(0, 0, 127, 1)',
+    //     //   'rgba(63, 0, 91, 1)',
+    //     //   'rgba(127, 0, 63, 1)',
+    //     //   'rgba(191, 0, 31, 1)',
+    //     //   'rgba(255, 0, 0, 1)'
+    //     // ],
+    //     radius: 100,
+    //     opacity: 0.7
+    //   }
+    // };
+    // console.log("Pre-out");
+    // setTimeout(() => {
+    //   this.show()
+    // }, this.props.wait);
+    // console.log("Post-out");
+    // this.setState({
+    //   //heatmapData: heatmap,
+    //   //countyData: this.props.countyData,
+    //   globalData: this.props.globalData
+    // });
+    //console.log("FINAL CHECK!");
+    //console.log(this.state.heatmapData);
+  }
+
+  mapWorld(){
     var heatmapPositions = [];
     var i = {};
     //console.log("Initial transmission");
     //console.log(this.props.heatmapData);
     for (i of this.props.heatmapData) {
-        heatmapPositions.push({
-          lat: i.Lat,
-          lng: i.Lon,
-          weight: i.TotalConfirmed
-        });
+      heatmapPositions.push({
+        lat: i.Lat,
+        lng: i.Lon,
+        weight: i.TotalConfirmed
+      });
     }
+    // console.log(this.props.countyData)
+    // var count = 0;
+    // for (i of this.props.countyData) {
+    //   heatmapPositions.push({
+    //     lat: i.Lat,
+    //     lng: i.Lon,
+    //     weight: i.CurrentCases
+    //   });
+    //   count++;
+    //   // if (count >) {
+    //   //   break;
+    //   // }
+    // }
     //console.log(heatmapPositions);
+    var heatmap = {
+      positions: heatmapPositions,
+      options: {
+        // gradient: [
+        //   'rgba(0, 255, 255, 0)',fullCountyData
+        //   'rgba(0, 255, 255, 1)',
+        //   'rgba(0, 191, 255, 1)',
+        //   'rgba(0, 127, 255, 1)',
+        //   'rgba(0, 63, 255, 1)',
+        //   'rgba(0, 0, 255, 1)',
+        //   'rgba(0, 0, 223, 1)',
+        //   'rgba(0, 0, 191, 1)',
+        //   'rgba(0, 0, 159, 1)',
+        //   'rgba(0, 0, 127, 1)',
+        //   'rgba(63, 0, 91, 1)',
+        //   'rgba(127, 0, 63, 1)',
+        //   'rgba(191, 0, 31, 1)',
+        //   'rgba(255, 0, 0, 1)'
+        // ],
+        radius: 100,
+        opacity: 0.7
+      }
+    };
+    console.log(heatmap);
+    this.setState({
+      heatmapData: heatmap,
+    });
+  }
+
+  mapAmerica() {
+    var heatmapPositions = [];
+    var i = {};
+    var count = 0;
+    for (i of countyData) {
+      heatmapPositions.push({
+        lat: i.lat,
+        lng: i.lon,
+        weight: i.weight
+      });
+      // count++;
+      // if (count > 100) {
+      //   break;
+      // }
+    }
+    console.log(heatmapPositions);
     var heatmap = {
       positions: heatmapPositions,
       options: {
@@ -62,12 +190,14 @@ export default class MapContainer extends Component {
     };
     this.setState({
       heatmapData: heatmap,
-      countyData: this.props.countyData,
-      globalData: this.props.globalData
     });
-    //console.log("FINAL CHECK!");
-    //console.log(this.state.heatmapData);
+    //console.log(countyData)
   }
+
+  show() {
+    this.setState({ hidden: false })
+  }
+
 
   renderMap(map, maps) {
     console.log("Map Rendered")
@@ -77,6 +207,7 @@ export default class MapContainer extends Component {
     console.log("Rendering map...");
     const { zoom, center, options, heatmapData } = this.state;
     return (
+      <div>{this.state.hidden ? null :
       <div style={{ height: '100vh', width: '100%' }}>
         <GoogleMapReact
           bootstrapURLKeys={apiKey}
@@ -89,6 +220,7 @@ export default class MapContainer extends Component {
           onGoogleApiLoaded={() => this.renderMap()}
         />
       </div>
+      }</div>
     );
   }
 }
--- src/dataset/processCountiesShort.py
+++ src/dataset/processCountiesShort.py
@@ -16,29 +16,44 @@ with open('currentCountyCases.csv') as file2:
             first = row[0]
             last = row[len(row)-2]
             county_dict = {
-                'County': row[1],
                 'FIPS': first,
-                'CurrentCases': last
+                'weight': int(last)
             }
             if not int(first) == 0: 
                 abridged_data.append(county_dict)
-            print(county_dict['CurrentCases'])
+            print(county_dict['weight'])
         line += 1
 
 for county in abridged_data:
     for i in county_data:
         if int(county['FIPS']) == int(i['FIPS']):
-            county['Lat'] = i['Latitude']
-            county['Lon'] = i['Longitude']
-            county['Population'] = i['Population']
-            county['State'] = i['State']
+            if str(i['Latitude'])[0] == '–':
+                county['lat'] = float('-' + str(i['Latitude'])[1:])
+            else:
+                county['lat'] = float(i['Latitude'])
+            if str(i['Longitude'])[0] == '–':
+                county['lon'] = float('-' + str(i['Longitude'])[1:])
+            else:
+                county['lon'] = float(i['Longitude'])
 
 # check over data for inconsistencies
 index = 0
 for county in abridged_data:
-    if not 'Lat' in county:
+    #print('YO')
+    abridged_data[index].pop('FIPS',None)
+    if 'FIPS' in county:
+        print('YO')
+        print(abridged_data[index]['FIPS'])
+        del county['FIPS']
+        county.pop('FIPS',None)
+        print("Yo")
+    try: 
+        del abridged_data[index]['FIPS']
+    except:
+        pass
+    if not 'lat' in county:
         del abridged_data[index]
     index += 1
 
-with open('fullCountyData.json', 'w') as outfile:
+with open('countyCoords.json', 'w') as outfile:
     outfile.write(json.dumps(abridged_data, indent=4, sort_keys=True))
